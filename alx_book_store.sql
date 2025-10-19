776d93fad7162fbee04fab4ae28558dfb393a94a
Barrack <barrackdome45@gmail.com>
2025-10-19 06:59:34 +0300

    Created alx_book_store database and all related tables (Books, Authors, Customers, Orders, Order_Details)

---
+++ alx_book_store.sql
@@ -0,0 +1,9 @@
+import mysql.connector
+connection = mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="#2025@MySQLL",
+    database="alx_book_store"
+)
+
+print(alx_book_store.get_server_info)
